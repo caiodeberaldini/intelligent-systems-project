b40a520c0270a8fc4596c310a5ac7d7a3fbe5590
Caio Deberaldini <caio-soad@hotmail.com>
2022-02-02 23:52:08 -0300

    API implementation

---
+++ server/api.py
@@ -0,0 +1,67 @@
+import os
+import re
+import sys
+import pickle
+
+import pandas as pd
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+from dotenv import load_dotenv
+
+from utils.process_text import processText
+
+_ = load_dotenv()
+
+MODEL_PATH = os.getenv("MODEL_PATH")
+VECTORIZER_PATH = os.getenv("VECTORIZER_PATH")
+LABEL_ENC_PATH = os.getenv("LABEL_ENC_PATH")
+
+app = Flask(__name__)
+
+@app.route('/<name>', methods=["GET"])
+def hello_world(name):
+    return {'Name': f'Hello, {name}!'}
+
+@app.route('/v1/categorize', methods=["POST"])
+def classifyProducts():
+    try:
+        content = request.get_json()
+    except:
+        return {"Message": "Unsupported input!"}, 400
+
+    products = [product['title'] for i, product in enumerate(content['products'])]
+
+    process_products = processText(pd.Series(products))
+
+    with open(MODEL_PATH, 'rb') as f:
+        model = pickle.load(f)
+        f.close()
+
+    with open(VECTORIZER_PATH, 'rb') as f:
+        vectorizer = pickle.load(f) 
+        f.close()
+
+
+    with open(LABEL_ENC_PATH, 'rb') as f:
+        le = pickle.load(f) 
+        f.close()
+
+    proc_prods = vectorizer.transform(process_products)
+
+    results = [model.predict(prod)[0] for prod in proc_prods]
+    results = list(le.inverse_transform(results))
+    results = {"categories": results}
+
+    # Assessment of the model's test accuracy (test purpose only!)
+    # with open('./metrics_test.txt', 'w+') as f:
+    #     categories = [product['category'] for i, product in enumerate(content['products'])]
+    #     metrics = 100*model.score(proc_prods, categories)
+    #     metrics = f'Accuracy (Test data): {metrics:.2f}%'
+
+    #     f.write(metrics)
+    #     f.close()
+
+    return jsonify(results)
